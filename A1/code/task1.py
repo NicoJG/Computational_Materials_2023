d13640a37ce800f9dc2bcf51575fe9983f35d62b
ErCaNa <ercanalevin@gmail.com>
2023-01-24 16:57:59 +0100

    task1 fix

--- A1/code/task1.py
+++ A1/code/task1.py
@@ -9,16 +9,17 @@ def spq(alpha1, alpha2):
     return np.pi**(3/2)/((alpha1+alpha2)**(3/2))
 
 def hpq(p, q):
-    hp = -4*np.pi/(p+q)**2+3*np.pi**(3/2)*p*q/(p+q)**(5/2)-4*np.pi*q/(p+q)**2
+    hp = -4*np.pi/(p+q)+3*np.pi**(3/2)*p*q/(p+q)**(5/2)
+
     #hp = 3*np.pi*q*np.sqrt(np.pi/(p+q)**3)-3*np.pi*q**2*np.sqrt(np.pi/(p+q)**5)-4*np.pi/(p+q)
-    #hp = (np.pi**(3/2) * alpha1 *alpha2 * np.sqrt(alpha1+alpha2)- 4*alpha2**2 - 8*alpha1*alpha2 - 4*alpha1**1)/ \
-    #    (alpha1**3+alpha2**3+3*alpha1**2*alpha2+3*alpha2**2*alpha1)
+    #hp = (np.pi**(3/2) * p *q * np.sqrt(p+q)- 4*q**2 - 8*p*q - 4*p**1)/ \
+    #   (p**3+q**3+3*p**2*q+3*q**2*p)
     #hp = -np.pi**(3/2)*(alpha2**2+5*alpha1*alpha2+4*alpha1**2-3)/ \
-    #    (np.sqrt(alpha1+alpha2)*(alpha2**2+2*alpha1*alpha2+alpha1**2))
+    #   (np.sqrt(alpha1+alpha2)*(alpha2**2+2*alpha1*alpha2+alpha1**2))
     return hp
             
 def qprqs(alpha1,alpha2,alpha3,alpha4):
-    qprqs = 2*np.pi**(5/2)/((alpha1+alpha3)*(alpha2*alpha4)*np.sqrt(alpha1+alpha2+alpha3+alpha4))
+    qprqs = 2*np.pi**(5/2)/((alpha1+alpha3)*(alpha2+alpha4)*np.sqrt(alpha1+alpha2+alpha3+alpha4))
     return qprqs
 
 def normalize_vec(vector):
@@ -55,13 +56,17 @@ for i in range(len(q_matrix)):
         for k in range(len(q_matrix)):
             for l in range(len(q_matrix)):
                 q_matrix[i,j,k,l] = qprqs(alphas[i],alphas[j],alphas[k], alphas[l])
+print("Q")
+print(q_matrix[1][3])
+
 
 # initialize  C-vector and eigenvaluevector
 c_vec = np.ones((1,4))
 c_vec=normalize_vec(c_vec)
 eg_vec = np.array([0])
 
-for i in range(10):
+for i in range(1000):
+    
     # some broadcasting magic to get the sum 
 
     q_matrix_sum1 = q_matrix*c_vec.reshape(1,-1,1,1)*c_vec.reshape(1,1,1,-1)
@@ -72,15 +77,15 @@ for i in range(10):
     #for r in range(4):
     #    for s in range(4):
     #        q_matrix_sum1 += q_matrix[:,r,:,s]*c_vec[0,r]*c_vec[0,s]
-    #print(q_matrix_sum2)
-    #print(q_matrix_sum1)
+    
 
     # Get  matrix
     f_matrix = h_matrix + q_matrix_sum1
 
     # Solve generalized eigenvalue problem and obtain eigenvector
-
+    
     eigenvalues, eigenvectors = np.linalg.eig(np.linalg.inv(s_matrix)@f_matrix)
+    
     min_index = np.argmin(eigenvalues)
     c_vec = eigenvectors[:,min_index].reshape((1,4))
     c_vec=normalize_vec(c_vec)
@@ -100,6 +105,7 @@ for i in range(10):
 
 
 print(eg_vec[-1])
+
 
 # %%
 
