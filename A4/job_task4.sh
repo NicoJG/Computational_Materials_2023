854d8e0c6606c11fcbffc06f91ce28ac3b096b6e
Nico Guth <nicog@chalmers.se>
2023-02-22 19:40:06 +0100

    A4 task 4

---
+++ A4/job_task4.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH -A C3SE2023-2-2 # Project
+#SBATCH -J A4Task4 # Name of the job
+#SBATCH -N 1 # Use 1 node
+#SBATCH -n 24 #
+#SBATCH -t 01:00:00 # Maximum time
+#SBATCH -o task4output/std.out # stdout goes to this file
+#SBATCH -e task4output/err.out # stderr goes to this file
+
+# Unload all modules, to make sure no incompatibilities arise
+module purge
+
+# Load desired modules
+module load GPAW/22.8.0-foss-2022a
+
+# Run program
+srun -u gpaw python task4.py
---
+++ A4/task4.py
@@ -0,0 +1,22 @@
+from ase import Atoms
+from gpaw import GPAW, PW
+from ase.units import eV
+from ase.parallel import world,parprint
+
+magmom = {"O2":[1.7,1.7], "CO":[2.5,1.7]}
+
+for molecule in ["O2","CO"]:
+    atoms = Atoms(molecule, [(-1, 0, 0), (1, 0, 0)])
+    atoms.set_cell([12.0,12.0,12.0])
+    atoms.set_initial_magnetic_moments(magmom[molecule])
+    atoms.center()
+
+    atoms.calc = GPAW(xc = 'PBE',
+                    mode=PW(450),
+                    kpts =(1, 1, 1),
+                    spinpol=True,
+                    txt = f"task4output/gpaw_output_{molecule}.txt")
+
+    E = atoms.get_potential_energy()*eV
+
+    parprint(f"Energy of {molecule}: {E:.5f} eV")
