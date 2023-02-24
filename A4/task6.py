a3ca433cea6eab29b2cdbb52523fb99bbce33f0d
Nico Guth <nicog@chalmers.se>
2023-02-24 12:09:53 +0100

    A4 running task6, not done!

--- A4/task6.py
+++ A4/task6.py
@@ -1,16 +1,17 @@
-# %%
+import time
+start_time = time.time()
+
 from ase.build import fcc111, add_adsorbate, molecule
 from ase.units import eV,J,m
 from ase.parallel import world,parprint
 from tqdm.auto import tqdm
-from ase.visualize import view
 from ase.io import write
 from gpaw import GPAW, PW
 from ase.optimize import GPMin
 from ase.constraints import FixAtoms
 
 from pathlib import Path
-Path("task5output/structures").mkdir(exist_ok=True)
+Path("task6output/structures").mkdir(exist_ok=True)
 
 a0 = {
     "Au": 4.178,
@@ -21,7 +22,7 @@ a0 = {
 combinations = [("Au","O"),("Pt","O"),("Rh","O"),
                 ("Au","CO"),("Pt","CO"),("Rh","CO")]
 
-for metal,adsorbate in tqdm(combinations):
+for metal,adsorbate in tqdm(combinations, disable=(world.rank!=0)):
     atoms = fcc111(metal, a=a0[metal], size=(3, 3, 3), vacuum=6.0)
     atoms.pbc = True
     
@@ -40,14 +41,19 @@ for metal,adsorbate in tqdm(combinations):
     atoms.calc = GPAW(xc = 'PBE',
                     mode=PW(450),
                     kpts =(12,12,12),
+                    spinpol=True,
                     txt = f"task6output/gpaw_output_{metal}_{adsorbate}.txt")
     
     dyn = GPMin(atoms, trajectory=f'task6output/structures/relax_{metal}_{adsorbate}.traj', logfile=f'task6output/structures/relax_{metal}_{adsorbate}.log')
     dyn.run(fmax=0.1, steps=1000)
+
+    E = atoms.get_potential_energy()
+
+    parprint(f"Energy of {metal}+{adsorbate}: {E:.5f} eV")
     
     write(f"task6output/structures/{metal}_{adsorbate}_after.xyz", atoms)
     
-    
+parprint(f"Runtime: {time.time()-start_time} seconds")
     
 
 
