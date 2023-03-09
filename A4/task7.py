ddf7c4f458cd75467a6c3083283cbe805dd39fec
Nico Guth <nicog@chalmers.se>
2023-03-09 17:41:36 +0100

    A4 task 7 pretty much done

--- A4/task7.py
+++ A4/task7.py
@@ -19,8 +19,10 @@ with open("results.json", "r") as file:
 
 k_B = 8.617333e-5 # eV/K
 
-S_O2 = results["entropy_O2"]
-S_CO = results["entropy_CO"]
+#S_O2 = results["entropy_O2"]
+#S_CO = results["entropy_CO"]
+
+T, S_O2, S_CO = np.genfromtxt("task7output/task7_entropies.csv", delimiter=",", unpack=True)
 
 E_O2 = results["E_mol_O2"]
 E_CO = results["E_mol_CO"]
@@ -42,7 +44,7 @@ def theta_CO(T, E_ads_O, E_ads_CO):
 
 def k_3(T, E_a):
     nu = 1e12
-    return np.exp(-E_a/(k_B*T))
+    return nu*np.exp(-E_a/(k_B*T))
 
 def r_3(T, theta_O_, theta_CO_, E_a):
     return theta_O_*theta_CO_*k_3(T,E_a)
@@ -50,11 +52,10 @@ def r_3(T, theta_O_, theta_CO_, E_a):
 # %%
 theta_O_dict = {}
 theta_CO_dict = {}
+r_3_atomic_units_dict = {}
 r_3_dict = {}
 
 for metal in ["Au","Pt","Rh"]:
-    T = np.linspace(100,2000, 1901)
-
     E_ads_O = results[f"E_ads_{metal}_O"]
     E_ads_CO = results[f"E_ads_{metal}_CO"]
     E_a = results[f"E_a_{metal}"]
@@ -63,42 +64,67 @@ for metal in ["Au","Pt","Rh"]:
     theta_CO_ = theta_CO(T, E_ads_O, E_ads_CO)
     r_3_ = r_3(T, theta_O_, theta_CO_, E_a)
     
+    sites_per_area = 9/results[f"surface_area_{metal}"] # Å^-2
+
+    mol = 6.02214076e23 # atoms
+    m = 1e10 # Å
+
     theta_O_dict[metal] = theta_O_
     theta_CO_dict[metal] = theta_CO_
-    r_3_dict[metal] = r_3_
+    r_3_atomic_units_dict[metal] = r_3_ # s^-1
+    r_3_dict[metal] = r_3_*sites_per_area / mol * 1e20 # mol m^-2 s^-1
 
 # %%
-plt.figure()
+# Find the maxima
 for metal in ["Au","Pt","Rh"]:
-    plt.plot(T, theta_O_dict[metal], label=metal)
+    idx_max = np.argmax(r_3_dict[metal])
+    results[f"r_3_max_{metal}[mol Ang^-2 s^-1]"] = r_3_dict[metal][idx_max]
+    results[f"T(r_3_max_{metal})[K]"] = T[idx_max]
+
+with open("results.json",'w') as json_file:
+    json.dump(results, json_file, indent=4)
+
+# %%
+plt.figure(figsize=(8,6))
+for i,metal in enumerate(["Au","Pt","Rh"]):
+    plt.plot(T, theta_O_dict[metal], f"C{i}--", label=fr"$\theta_O\,$ ({metal})")
+    plt.plot(T, theta_CO_dict[metal], f"C{i}-", label=fr"$\theta_{{CO}}$({metal})")
 plt.yscale("log")
 plt.xlabel("Temperature (K)")
-plt.ylabel("Fractional coverage of O")
+plt.ylabel("Fractional coverage")
+plt.ylim(1e-15,10)
 plt.grid()
 plt.legend()
-plt.savefig("plots/task7_theta_O.pdf")
+plt.tight_layout()
+plt.savefig("plots/task7_theta.pdf")
 
-plt.figure()
+plt.figure(figsize=(8,6))
 for metal in ["Au","Pt","Rh"]:
-    plt.plot(T, theta_CO_dict[metal], label=metal)
+    plt.plot(T, r_3_atomic_units_dict[metal], label=metal)
+plt.ylim(bottom=r_3_atomic_units_dict["Au"].min()*100, top=r_3_atomic_units_dict["Pt"].max()*10)
 plt.yscale("log")
 plt.xlabel("Temperature (K)")
-plt.ylabel("Fractional coverage of CO")
+plt.ylabel(r"Formation rate of $\mathrm{CO}_2$ ($\mathrm{s}^{-1}$)")
 plt.grid()
 plt.legend()
-plt.savefig("plots/task7_theta_CO.pdf")
+plt.tight_layout()
+plt.savefig("plots/task7_r_3_atomic_units.pdf")
 
-plt.figure()
-for metal in ["Au","Pt","Rh"]:
-    plt.plot(T, r_3_dict[metal], label=metal)
-
+plt.figure(figsize=(8,6))
+for i,metal in enumerate(["Au","Pt","Rh"]):
+    r_max = results[f"r_3_max_{metal}[mol Ang^-2 s^-1]"]
+    T_max = results[f"T(r_3_max_{metal})[K]"]
+    plt.plot(T, r_3_dict[metal], fr"C{i}-", label=f"{metal} ({T_max:.0f} K, {r_max:.3e})")
+    plt.plot(T_max, r_max, f"C{i}x")
 plt.ylim(bottom=r_3_dict["Au"].min()*100, top=r_3_dict["Pt"].max()*10)
 plt.yscale("log")
 plt.xlabel("Temperature (K)")
-plt.ylabel(r"Formation rate of $\mathrm{CO}_2$")
+plt.ylabel(r"Formation rate of $\mathrm{CO}_2$ (mol $\mathrm{m}^{-2}$ $\mathrm{s}^{-1}$)")
 plt.grid()
 plt.legend()
+plt.tight_layout()
 plt.savefig("plots/task7_r_3.pdf")
+
 
 
 # %%
--- A4/task7_entropies.py
+++ A4/task7_entropies.py
@@ -20,43 +20,40 @@ T = np.linspace(100,2000,1901)
 S_dict = {}
 
 for molec in ["O2","CO"]:
+    parprint(f"==========================")
+    parprint(f"Start calculations for {molec}...")
+    atoms = molecule(molec, vacuum=6.0)
+    atoms.pbc = True
+    atoms.calc = GPAW(xc = 'PBE',
+                    mode=PW(450),
+                    kpts =(1, 1, 1),
+                    spinpol=True,
+                    symmetry={'point_group': False}, # Turn off point-group symmetry
+                    txt = None)
+    potentialenergy = atoms.get_potential_energy()*eV
+
+    vib = Vibrations(atoms)
+    vib.run()
+    vib_energies = vib.get_energies()
+
+    thermo = IdealGasThermo(vib_energies=vib_energies,
+                            potentialenergy=potentialenergy,
+                            atoms=atoms,
+                            geometry='linear', 
+                            spin=spin[molec],
+                            symmetrynumber = symmetry_number[molec])
+    
+    parprint(f"Vibrational energies: {vib_energies}")
+    parprint(f"Potential Energy: {potentialenergy:.5f} eV")
     parprint(f"Entropies for {molec}...")
+
     S_dict[molec] = []
     for T_i in tqdm(T, disable=(world.rank!=0)):
-        atoms = molecule(molec, vacuum=6.0)
-        atoms.pbc = True
-        atoms.calc = GPAW(xc = 'PBE',
-                        mode=PW(450),
-                        kpts =(1, 1, 1),
-                        spinpol=True,
-                        symmetry={'point_group': False}, # Turn off point-group symmetry
-                        txt = None)
-        potentialenergy = atoms.get_potential_energy()*eV
-
-        vib = Vibrations(atoms)
-        vib.run()
-        vib_energies = vib.get_energies()
-
-        thermo = IdealGasThermo(vib_energies=vib_energies,
-                                potentialenergy=potentialenergy,
-                                atoms=atoms,
-                                geometry='linear', 
-                                spin=spin[molec],
-                                symmetrynumber = symmetry_number[molec])
-        S = thermo.get_entropy(temperature=300, pressure=100000)
-
+        S = thermo.get_entropy(temperature=T_i, pressure=100000, verbose=False)
         S_dict[molec].append(S)
-
-        parprint(f"---------------------------")
-        parprint(f"Molecule: {molec}")
-        parprint(f"Temperature: {T_i:.2f} K")
-        parprint(f"Vibrational energies: {vib_energies}")
-        parprint(f"Potential Energy: {potentialenergy:.5f} eV")
-        parprint(f"Entropy: {S:.5f} eV/K")
-        parprint(f"Entropy: {S*mol/J:.5f} J/mol K")
-        parprint(f"Total runtime: {time.time()-start_time}")
+        parprint(f"Entropy at {T_i:.1f} K: {S:.5f} eV/K \t = {S*mol/J:.5f} J/mol K")
 
 if world.rank == 0:
-    data = list(zip(T, S_dict["O"], S_dict["CO"]))
-    np.savetxt("task7output/task7_entropies.csv", data, header="# T[K], S_O[eV/K], S_CO[eV/K]", comments="", fmt= "%.10f, %.10f, %.10f")
+    data = list(zip(T, S_dict["O2"], S_dict["CO"]))
+    np.savetxt("task7output/task7_entropies.csv", data, header="# T[K], S_O2[eV/K], S_CO[eV/K]", comments="", fmt= "%.10f, %.10f, %.10f")
     print("Runtime",time.time()-start_time)
